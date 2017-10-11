7095911275c79fa097783f2fc63d7f0b3f37d8ec
Pedram Mokarrami <pedrammokarrami@gmail.com>
2017-10-11 13:44:58 +0330

    Initial Commit

---
+++ zizmart/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  zizmart
+//
+//  Created by sarir on 7/19/1396 AP.
+//  Copyright © 1396 zizmart. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
