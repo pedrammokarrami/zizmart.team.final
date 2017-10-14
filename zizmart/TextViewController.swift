42601f1823512e67a24e967820dbf128cf038edc
Pedram Mokarrami <pedrammokarrami@gmail.com>
2017-10-14 17:47:21 +0330

    home

---
+++ zizmart/TextViewController.swift
@@ -0,0 +1,23 @@
+//
+//  Text.swift
+//  zizmart
+//
+//  Created by sarir on 7/22/1396 AP.
+//  Copyright © 1396 zizmart. All rights reserved.
+//
+
+import UIKit
+
+class TextViewController: UIViewController {
+    @IBAction func text(_ sender: Any) {
+    }
+    @IBOutlet weak var sabt: UITextField!
+    override func viewDidLoad()  {
+    super.viewDidLoad()
+    sabt.leftView 
+    
+    
+    
+    {
+    }
+}
