72aed5b0fa6e15e3faa6afafcd939dd16cc70fce
Pedram Mokarrami <pedrammokarrami@gmail.com>
2017-10-18 16:01:22 +0330

    finish barcode

--- zizmart/QRCodeViewController.swift
+++ zizmart/QRCodeViewController.swift
@@ -8,18 +8,26 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
-
+class QRCodeViewController: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // Do any additional setup after loading the view.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
+    
+    // MARK: - Navigation
+    
+    @IBAction func unwindToHomeScreen(segue: UIStoryboardSegue) {
+        dismiss(animated: true, completion: nil)
+    }
+    
 }
 
---
+++ zizmart/QRScannerController.swift
@@ -0,0 +1,38 @@
+//
+//  QRScannerController.swift
+//  zizmart
+//
+//  Created by sarir on 7/26/1396 AP.
+//  Copyright © 1396 zizmart. All rights reserved.
+//
+
+import UIKit
+
+class QRScannerController: UIViewController {
+    
+    @IBOutlet var messageLabel:UILabel!
+    @IBOutlet var topbar: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
+}
