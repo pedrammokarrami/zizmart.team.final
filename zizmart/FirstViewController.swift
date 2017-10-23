d423a8f3c118d0ffa495ac119defc63cf3a8ff3c
Pedram Mokarrami <pedrammokarrami@gmail.com>
2017-10-23 14:06:21 +0330

    gand zadam

--- zizmart/FirstViewController.swift
+++ zizmart/FirstViewController.swift
@@ -13,7 +13,7 @@ class FirstViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    }
+//    }M,bnretiqyiuort3jtruefifhefljl;fhrfyejlkfejhhyefwjewjrujefghh
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
