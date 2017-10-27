280f3a04f32a98c2a3dbd103908b1a1e5d9a469e
Pedram Mokarrami <pedrammokarrami@gmail.com>
2017-10-28 01:50:13 +0330

    apple map

---
+++ zizmart/MapViewController..swift
@@ -0,0 +1,28 @@
+//
+//  Map.swift
+//  
+//
+//  Created by sarir on 8/4/1396 AP.
+//
+
+import UIKit
+import MapKit
+import CoreLocation
+
+
+
+class MapViewController: UIViewController, CLLocationManagerDelegate {
+    
+//    map
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+       
+}
+
+}
